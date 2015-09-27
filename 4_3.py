0e865dc0da173180063c31440bbd5899dc6fa40b
BooYang-CS <BooYang-CS@outlook.com>
2015-09-27 10:05:26 +0800

    20150927

--- 4_3.py
+++ 4_3.py
@@ -63,7 +63,7 @@ def count():
 	for i in range(1,4):
 		def f():
 			return i*i
-		fs.append(f(i))
+		fs.append(f)
 	return fs
 f1,f2,f3=count()
 print(f1())
@@ -83,7 +83,7 @@ def count():
 		return g
 	fs=[]
 	for i in range(1,4):
-		fs.append(f(i))
+		fs.append(f(i))#f(i)立刻执行，因此i的当前值被传入f()
 	return fs
 f1,f2,f3=count()
 print(f1())
