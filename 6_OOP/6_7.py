3ab87699991c5df91f638ad62b799eaf9085671d
BooYang-CS <BooYang-CS@outlook.com>
2015-10-12 16:47:38 +0800

    20151011

--- 6_OOP/6_7.py
+++ 6_OOP/6_7.py
@@ -81,7 +81,7 @@ __new__()方法接收到的参数依次是：
 1.当前准备创建的类的对象
 2.类的名字
 3.类继承的父类集合
-4.类的方法集合 attrs['add','plus']=lambda self,value:self.append(value),lambda self,value:append(vlaue*2)
+4.类的方法集合 attrs['add','plus']=lambda self,value:self.append(value)
 '''
 L=Mylist()
 L.add(1)
